ac80022557cc4e04ddcf237b24d45d3a84dec4df
Janaky Murthy <janaky96@yahoo.com>
2021-07-25 16:43:52 +0530

    ./workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -7,7 +7,8 @@ notablog generate .;
 cp -r public/. $cur_dir/.;
 cd $cur_dir;
 git add .;
-commit_msg="${0:-'regenerated blog'}"
+commit_msg="${0:-'regenerated blog'}";
+echo $commit_msg;
 git commit -m $commit_msg;
 git push -u origin main;
 #cd ../; notablog generate .; cd janakymurthy.github.io; cp -r ../public/ .; git add .; git commit -m "re generated blog"; git push -u origin main
